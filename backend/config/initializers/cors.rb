2bbe235b8eef0b7c1cb61874eda77c25752faf5f
Chistikov Maksim <97792122+maksimka1267@users.noreply.github.com>
2025-11-07 23:01:50 +0200

    Update cors.rb

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,16 +1,14 @@
-# Be sure to restart your server when you modify this file.
+# config/initializers/cors.rb
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Откуда разрешаем запросы:
+    origins "https://frontend-5v01.onrender.com",
+            "http://localhost:3000"   # для локальной разработки
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    # Какие ресурсы и методы разрешены
+    resource "*",
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             max_age: 600
+  end
+end
